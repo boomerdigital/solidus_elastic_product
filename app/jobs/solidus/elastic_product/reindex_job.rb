117caa9a2e6d01e3d916080e52afe89e22cdcb4d
Martin Tomov <mtomov@outlook.com>
2016-12-20 14:41:10 +0000

    Add Reindex Job to perform a full index
      - Dynamically swap the active index with the new one using aliases once
    
        the new index has successfully been created and populated
    
        (borrowed from Searchkick gem)
    
    
      - Reindex should be run anytime the mappings change
    
    
      - Reindex does not serialize products. Use `Schedule.serialize_all` on
    
        the first run to serialize them

---
+++ app/jobs/solidus/elastic_product/reindex_job.rb
@@ -0,0 +1,74 @@
+# Creates a new index, populates it with all products, and
+# finally swaps the alias to the new index. Non-serialized products
+# will not be uploaded. Use Schedule.serialize_all on the first run
+# to serialize all products.
+# In all other cases, the incremental SerializerJob
+# should keep the records serialized.
+
+module Solidus::ElasticProduct
+  class ReindexJob < ActiveJob::Base
+    queue_as :elastic_uploader
+
+    delegate  :client, :index_name, :document_type,
+              :import, :create_index!, to: Index
+
+    def perform
+      old_state = Config.incremental_update_enabled
+      Config.incremental_update_enabled = false
+
+      new_index_name = index_name + "_" + Time.now.strftime('%Y%m%d%H%M%S%L')
+      create_index!(index: new_index_name)
+
+      # Put all data in the new index
+      import(index: new_index_name) do |response|
+        report_on(response)
+
+        # Mark successfully indexed products as uploaded
+        ids = response['items'].select { |p| p['index']['error'].nil? }
+                               .map { |p| p['index']['_id'] }
+        State.where(id: ids).mark_uploaded!
+      end
+
+      # Then assign the alias to the new index
+      swap(new_index_name)
+
+      # TODO: Do some old indices cleanup
+    ensure
+      # Restore to previous value (likely enabled)
+      Config.incremental_update_enabled = old_state
+    end
+
+  private
+
+    # Moves the alias to the new index, or
+    # creates one if an alias doesn't exist yet
+    #
+    # borrowed from searchkick/lib/searchkick/index.rb:36
+    def swap(new_name)
+      actions = []
+
+      if client.indices.exists_alias(name: index_name)
+        old_index_names = client.indices.get_alias(name: index_name).keys
+        actions += old_index_names.map do |old_name|
+          { remove: { index: old_name, alias: index_name } }
+        end
+      end
+
+      actions += [{ add: { index: new_name, alias: index_name } }]
+      client.indices.update_aliases body: { actions: actions }
+    end
+
+    def report_on(response)
+      if response["errors"]
+        Rails.logger.error "ElasticProduct: Failed to perform reindex"
+        response['items'].each do |p|
+          message = { id: p['index']['_id'] }.merge p['index']['error']
+          Rails.logger.error message
+        end
+      else
+        Rails.logger.info "ElasticProduct: Reindexed successfully in #{response['took']} ms"
+      end
+    end
+
+  end
+end
---
+++ spec/jobs/solidus/elastic_product/reindex_job_spec.rb
@@ -0,0 +1,138 @@
+require 'spec_helper'
+
+module Solidus::ElasticProduct
+  describe ReindexJob do
+    let(:indices) { instance_double(Elasticsearch::API::Indices::IndicesClient) }
+    let(:client) { instance_double(Elasticsearch::Transport::Client, indices: indices ) }
+
+    before do
+      allow(Index).to receive(:client).and_return client
+      allow(Index).to receive(:create_index!)
+    end
+
+
+    describe 'restoring incremental state' do
+      let(:indices) { double('indices', get_alias: {}).as_null_object }
+
+      before do
+        allow(Index).to receive(:import)
+      end
+
+      it 'will return the incremental state to the old state when done' do
+        subject.perform
+        expect( Config.incremental_update_enabled ).to eq true
+      end
+
+      it 'will restore if previously false' do
+        begin
+          Config.incremental_update_enabled = false
+          subject.perform
+          expect( Config.incremental_update_enabled ).to eq false
+        ensure
+          Config.incremental_update_enabled = true
+        end
+      end
+    end
+
+    describe "#perform" do
+      context 'on success' do
+        let(:success_response) do
+          {
+            "took" => 85,
+            "errors" => false,
+            "items" => [
+              {  # [0]
+                "index" => {
+                     "_index" => "products_development_20161220135033355",
+                      "_type" => "spree/product",
+                        "_id" => "10",
+                   "_version" => 1,
+                     "result" => "created",
+                    "_shards" => {
+                            "total" => 2,
+                       "successful" => 1,
+                           "failed" => 0
+                   },
+                    "created" => true,
+                     "status" => 201
+                }
+              },
+              {  # [1]
+                "index" => {
+                     "_index" => "products_development_20161220135033355",
+                      "_type" => "spree/product",
+                        "_id" => "20",
+                   "_version" => 1,
+                     "result" => "created",
+                    "_shards" => {
+                            "total" => 2,
+                       "successful" => 1,
+                           "failed" => 0
+                   },
+                    "created" => true,
+                     "status" => 201
+                }
+              }
+            ]
+          }
+        end
+
+        it 'will upload all products using #import and mark them as uploaded' do
+          allow(Index).to receive(:import).and_yield success_response
+          expect(State).to receive(:where).with(id: ["10", "20"]).and_return State
+          expect(State).to receive(:mark_uploaded!)
+
+          expect(indices).to receive(:exists_alias).with({ name: 'products_test'}).and_return true
+          expect(indices).to receive(:get_alias).with({ name: 'products_test'}).and_return ({ 'old_index_name' => {} })
+          expect(indices).to receive(:update_aliases).
+            with({:body=>{:actions=>[
+              {:remove=>{:index=>"old_index_name", :alias=>"products_test"}},
+              {:add=>{:index=>/products_test/, :alias=>"products_test"}}
+            ]}})
+
+          subject.perform
+        end
+      end
+
+      context 'on errors' do
+        let(:error_response) do
+          {
+            "took" => 2,
+          "errors" => true,
+           "items" => [
+              { # [0]
+                "index" => {
+                    "_index" => "products_development_20161220134534623",
+                     "_type" => "spree/product",
+                       "_id" => "1",
+                    "status" => 400,
+                     "error" => {
+                             "type" => "mapper_parsing_exception",
+                           "reason" => "failed to parse",
+                        "caused_by" => {
+                              "type" => "not_x_content_exception",
+                            "reason" => "Compressor detection can only be called on some xcontent bytes or compressed xcontent bytes"
+                        }
+                    }
+                }
+              },
+            ]
+          }
+        end
+
+        it 'not mark failed to upload products as uploaded, but will still swap the index (may change in the future)' do
+          allow(Index).to receive(:import).and_yield error_response
+          expect(State).to receive(:where).with(id: []).and_return State
+          expect(State).to receive(:mark_uploaded!)
+
+          expect(indices).to receive(:exists_alias)
+          expect(indices).to receive(:update_aliases)
+
+          subject.perform
+        end
+
+      end
+    end
+
+  end
+end

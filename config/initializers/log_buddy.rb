5807399d188e41d8a6e729ed1eb0fcbfd3f4d63c
Martin Tomov <mtomov@outlook.com>
2016-12-29 17:34:06 +0000

    Remove LogBuddy from main app
      - best to locate the initializer in the dummy test app

--- config/initializers/log_buddy.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.development? || Rails.env.test?
-  require "log_buddy"
-  require "awesome_print"
-
-  LogBuddy.init(
-    logger: Logger.new('log/awesome.log'),
-    use_awesome_print: true
-  )
-end

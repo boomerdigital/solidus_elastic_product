7343cc574a0d96b28018f88cc88693bc1ae4ca86
Martin Tomov <mtomov@outlook.com>
2016-12-29 22:11:42 +0000

    Correct migration to handle postgres pecularities
       - run INSERT outside of a transaction
    
       - specify partial index on NULL for the `json` column
    
       - specify single indices for each of the datetime columns

--- db/migrate/20161023220641_create_elastic_product_state.rb
+++ db/migrate/20161023220641_create_elastic_product_state.rb
@@ -3,12 +3,17 @@ class CreateElasticProductState < ActiveRecord::Migration
     create_table :solidus_elastic_product_states do |t|
       t.belongs_to :product, null: false, index: true, unique: true
       t.text :json, limit: (16.megabytes - 1)
-      t.boolean :uploaded, default: false, null: false
-      t.datetime :locked_for_serialization_at, :locked_for_upload_at
+      t.boolean :uploaded, default: false, null: false, index: true
+      t.datetime :locked_for_serialization_at
+      t.datetime :locked_for_upload_at, index: true
 
-      t.index [:json, :locked_for_serialization_at], name: 'serialization_lookup', length: { json: 1 }
-      t.index [:uploaded, :locked_for_upload_at, :json], name: 'upload_lookup', length: { json: 1 }
+      t.index :locked_for_serialization_at, name: 'index_states_on_locked_for_serialization_at'
+      t.index :json, where: 'json is null'
     end
+
+    # to get out of a transaction
+    execute("commit;")
+
     execute "INSERT INTO #{Solidus::ElasticProduct::State.table_name} (product_id) SELECT id FROM #{Spree::Product.table_name}"
   end
 
